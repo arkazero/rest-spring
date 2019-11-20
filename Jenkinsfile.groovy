b8c053e1ef24285e5ec06a57895e3da0e6e02ce4
Wilmer Aguilera <waguiler@redhat.com>
2019-11-20 08:17:49 -0500

    12

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -137,11 +137,10 @@ node{
 				echo "Termina Deploy"
 			}
 		
-		
 		}catch(e){
-        statusProcess = "Proceso con error"
-        errorMessage = e.toString()
-        throw e
+	        statusProcess = "Proceso con error"
+	        errorMessage = e.toString()
+	        throw e
     }finally{
         //emailext(mimeType: 'text/html', replyTo: 'waguilera@redhat.com', subject: statusProcess+" : " + env.JOB_NAME, to: 'waguilera@redhat.com', body: statusProcess + " : " + env.JOB_NAME+ " : "+errorMessage)
 
