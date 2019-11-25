d37c9a68db3888fbd1fde82a7ad77e43986f366f
Wilmer Aguilera <waguiler@redhat.com>
2019-11-25 10:09:13 -0500

    Adicion de propiedades

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController
 {
+	@Value("${app.enviroment}")
+	private String enviroment;
+	
     @RequestMapping("/")
     public List<Customer> findAll()
     {
         List<Customer> customerList = new ArrayList<Customer>();
-        customerList.add(new Customer(1, "frank"));
-        customerList.add(new Customer(2, "john"));
+        customerList.add(new Customer(1, "wilmer"));
+        customerList.add(new Customer(2, "Sara"));
+        customerList.add(new Customer(2, "ENV: "+enviroment));
         return customerList;
     }
+
+	public String getEnviroment() {
+		return enviroment;
+	}
+
+	public void setEnviroment(String enviroment) {
+		this.enviroment = enviroment;
+	}
+    
 }
