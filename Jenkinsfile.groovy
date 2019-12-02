fb626c222576407538f31405611a0dc83edde8d9
Wilmer Aguilera <waguiler@redhat.com>
2019-12-02 15:10:47 -0500

    Pipeline sin version de build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -49,6 +49,8 @@ node{
 			
 			// Set the tag for the development image: version + build number
 			devTag  = "${version}-" + currentBuild.number
+			devTag  = "${version}"
+			
 			// Set the tag for the production image: version
 			prodTag = "${version}"
 		}
