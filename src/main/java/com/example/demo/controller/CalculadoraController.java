cf63967a3220a07cf291dac1ce3f2a9a99c4e941
Wilmer Aguilera <waguiler@redhat.com>
2019-12-05 17:20:47 -0500

    Propiedades

--- src/main/java/com/example/demo/controller/CalculadoraController.java
+++ src/main/java/com/example/demo/controller/CalculadoraController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +15,13 @@ public class CalculadoraController {
 	@Autowired
 	CalculadoraService calculadoraService;
 	
+
+	@Value( "${app.enviroment}" )
+	private String enviroment;
+	
 	@GetMapping
 	public String home() {
-		return "Prueba Calculadora 1.0";
+		return "Prueba Calculadora 1.0 - Env: "+enviroment;
 	}
 	
 	@RequestMapping("/sumar")
