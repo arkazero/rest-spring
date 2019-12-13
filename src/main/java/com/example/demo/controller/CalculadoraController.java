024cabd4e67dd48ba3a2ac89b816487b71642e70
Wilmer Aguilera <waguiler@redhat.com>
2019-12-13 15:32:51 -0500

    Ajuste version 1.0

--- src/main/java/com/example/demo/controller/CalculadoraController.java
+++ src/main/java/com/example/demo/controller/CalculadoraController.java
@@ -24,7 +24,7 @@ public class CalculadoraController {
 		return "Calculadora 2.0 - Env: "+nameApp;
 	}
 
-	@GetMapping("/healthcheck")
+	@GetMapping("/healthchec")
 	public String healthcheck() {
 		return "OK";
 	}
