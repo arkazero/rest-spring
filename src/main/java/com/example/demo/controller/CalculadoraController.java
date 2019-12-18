5569203627bec8083eb873d8e2cc158db0d4cde9
Wilmer Aguilera <waguiler@redhat.com>
2019-12-18 10:13:43 -0500

    Ajuste Bugs

--- src/main/java/com/example/demo/controller/CalculadoraController.java
+++ src/main/java/com/example/demo/controller/CalculadoraController.java
@@ -21,10 +21,10 @@ public class CalculadoraController {
 	
 	@GetMapping("/version")
 	public String home() {
-		return "Calculadora 3.0 - Env: "+nameApp;
+		return "Calculadora 3.1 - Env: "+nameApp;
 	}
 
-	@GetMapping("/healthcheck")
+	@GetMapping("/healthcheck2")
 	public String healthcheck() {
 		return "OK";
 	}
