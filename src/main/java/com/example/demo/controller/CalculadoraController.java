e34526debe6106f13868bcd442e8c3387c24d0a3
Wilmer Aguilera <waguiler@redhat.com>
2019-12-18 15:38:42 -0500

    Cambio de version

--- src/main/java/com/example/demo/controller/CalculadoraController.java
+++ src/main/java/com/example/demo/controller/CalculadoraController.java
@@ -21,7 +21,7 @@ public class CalculadoraController {
 	
 	@GetMapping("/version")
 	public String home() {
-		return "Calculadora 1.0 - Env: "+nameApp;
+		return "Calculadora 2.0 - Env: "+nameApp;
 	}
 
 	@GetMapping("/healthcheck")
