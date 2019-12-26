be31b632ca7c637859c98dd0143e6de9c05e067c
Wilmer Aguilera <waguiler@redhat.com>
2019-12-26 09:16:22 -0500

    Adicion de notificaciones por email

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -187,7 +187,7 @@ node{
 	        errorMessage = e.toString()
 	        throw e
     }finally{
-        //emailext(mimeType: 'text/html', replyTo: 'waguilera@redhat.com', subject: statusProcess+" : " + env.JOB_NAME, to: 'waguilera@redhat.com', body: statusProcess + " : " + env.JOB_NAME+ " : "+errorMessage)
+        emailext(mimeType: 'text/html', replyTo: 'waguilera@redhat.com', subject: statusProcess+" : " + env.JOB_NAME, to: 'waguilera@redhat.com', body: statusProcess + " : " + env.JOB_NAME+ " : "+errorMessage)
 
     }
 
