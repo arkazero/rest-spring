9f4935d968a613a6b2b55e2677de943ef02f0b55
Wilmer Aguilera <waguiler@redhat.com>
2020-02-27 21:26:33 -0500

    Eliminacion de código de carga

--- src/main/java/com/example/demo/controller/CalculadoraController.java
+++ src/main/java/com/example/demo/controller/CalculadoraController.java
@@ -1,13 +1,8 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,14 +32,6 @@ public class CalculadoraController {
     public String sumar(@RequestParam Integer sumando1, @RequestParam Integer sumando2){
     	Integer resultado = 0;
     	resultado = calculadoraService.sumar(sumando1, sumando2);
-    	
-    	try {
-			createFiles();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
         return ("Resultado: "+resultado);
     }
 
@@ -55,12 +42,4 @@ public class CalculadoraController {
 		return ("Resultado: "+resultado);
 	}
 	
-	private void createFiles() throws IOException {
-		for(int i = 0;i<10; i++) {
-			String data = "Test data";
-			String nameFile = "File"+Math.random();
-			Files.write(Paths.get("/deployments/data/"+nameFile), data.getBytes());
-		}
-	}
-
 }
--- src/main/java/com/example/demo/service/CalculadoraService.java
+++ src/main/java/com/example/demo/service/CalculadoraService.java
@@ -2,8 +2,6 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Vector;
-
 @Service
 public class CalculadoraService {
 	
@@ -15,34 +13,11 @@ public class CalculadoraService {
 	 */
 	public Integer sumar(Integer sumando1, Integer sumando2) {
 		Integer resultado = sumando1 + sumando2;
-
-		long start  = System.currentTimeMillis();
-		long count = 0l;
-		for(long x=0;x<Integer.MAX_VALUE ;x++){
-			count+=1;
-		}
-		long end = System.currentTimeMillis();
-		System.out.println(end-start +" ms");
-
 		return resultado;
 	}
 
 	public Integer restar(Integer minuendo, Integer sustraendo){
-
 		Integer resultado = minuendo + sustraendo;
-
-		System.out.println("El resultado de la rest es: "+resultado);
-
-
-		Vector v = new Vector();
-		for(int i = 0; i<50;i++)
-		{
-			byte b[] = new byte[1048576];
-			v.add(b);
-			Runtime rt = Runtime.getRuntime();
-			System.out.println( "free memory: " + rt.freeMemory() );
-		}
-
 		return resultado;
 	}
 
